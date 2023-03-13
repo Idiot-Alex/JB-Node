82a1f2a88b75928547adbf9fccd903bda4403bd0
Eleven <idiotalex@163.com>
2023-03-14 00:58:10 +0800

    remove imports

--- jbnode/src/test/java/com/hotstrip/jbnode/node/NodeServiceTest.java
+++ jbnode/src/test/java/com/hotstrip/jbnode/node/NodeServiceTest.java
@@ -1,22 +1,10 @@
 package com.hotstrip.jbnode.node;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.ConsoleAppender;
 import com.hotstrip.jbnode.JbNodeApplicationTests;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.helpers.MessageFormatter;
 
 import javax.annotation.Resource;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class NodeServiceTest extends JbNodeApplicationTests {
